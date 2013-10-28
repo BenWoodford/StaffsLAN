df5fc6a65c55768dfeb0c6dc7946975487398041
Ben Woodford <ben@tweedler.co.uk>
2013-10-28 17:24:18 +0000

    Allow the +

--- fuel/app/classes/extravalidation.php
+++ fuel/app/classes/extravalidation.php
@@ -2,7 +2,7 @@
 
 class ExtraValidation {
 	public static function _validation_phone_number($val) {
-		$pattern = "/^((\(?0\d{4}\)?\s?\d{3}\s?\d{3})|(\(?0\d{3}\)?\s?\d{3}\s?\d{4})|(\(?0\d{2}\)?\s?\d{4}\s?\d{4}))(\s?\#(\d{4}|\d{3}))?$/";
+		$pattern = "/^(\+44\s?7\d{3}|\(?07\d{3}\)?)\s?\d{3}\s?\d{3}$/";
 
 		$match = preg_match($pattern,$val);
 
