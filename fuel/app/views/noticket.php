e29961b035349d739ee2be55bc221788e041537b
Ben Woodford <ben@tweedler.co.uk>
2013-10-28 20:20:00 +0000

    Added prompt to refresh data

--- fuel/app/views/noticket.php
+++ fuel/app/views/noticket.php
@@ -9,6 +9,7 @@
 	<div class="box-content">
 		<?php if(empty($currentUser->student_number)) { ?>
 			<p>We couldn't find a ticket for you because we don't know your student card number! Please edit your <a href="http://staffsvgs.co.uk/account/personal-details#extrainfo">Personal Details</a> and enter your student number in the indicated box then try again.</p>
+			<p>If you're still having problems, click the <b>Refresh your Data</b> button in the top right corner of this page.</p>
 		<?php } else { ?>
 			<p>We couldn't find a ticket for your (using the student number <?=$currentUser->student_number;?>). Did you enter it correctly? If so, did you buy a ticket? If you didn't you can't buy them any more. :(</p>
 		<?php } ?>
