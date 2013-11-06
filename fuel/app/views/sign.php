8a44656535491f3d2eaf6d53569960ed1b97c37c
Ben Woodford <ben@tweedler.co.uk>
2013-11-06 22:33:34 +0000

    Added clearfix

--- fuel/app/views/sign.php
+++ fuel/app/views/sign.php
@@ -8,5 +8,6 @@
 	<div class="box-content text-center">
 		<a href="/sign/in" class="col-xs-12 col-md-5 col-sm-5 col-lg-5 btn btn-large btn-success"><i class="fa fa-sign-in"></i> Sign In</a>
 		<a href="/sign/out" class="col-xs-12 col-md-offset-2 col-lg-offset-2 col-sm-offset-2 col-md-5 col-sm-5 col-lg-5 btn btn-large btn-danger"><i class="fa fa-sign-out"></i> Sign Out</a>
+		<div class="clearfix"></div>
 	</div>
 </div>
