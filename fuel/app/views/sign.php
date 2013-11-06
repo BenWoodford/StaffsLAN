c7563d51b9a7088f8240a2835f6be643aefb2fab
Ben Woodford <ben@tweedler.co.uk>
2013-11-06 22:32:04 +0000

    Added spacing

--- fuel/app/views/sign.php
+++ fuel/app/views/sign.php
@@ -6,7 +6,7 @@
 		</h2>
 	</div>
 	<div class="box-content text-center">
-		<a href="/sign/in" class="col-md-6 col-sm-6 col-xs-12 col-lg-6 btn btn-large btn-success"><i class="fa fa-sign-in"></i> Sign In</a>
-		<a href="/sign/out" class="col-md-6 col-sm-6 col-xs-12 col-lg-6 btn btn-large btn-danger"><i class="fa fa-sign-out"></i> Sign Out</a>
+		<a href="/sign/in" class="col-xs-12 col-md-5 col-sm-5 col-lg-5 btn btn-large btn-success"><i class="fa fa-sign-in"></i> Sign In</a>
+		<a href="/sign/out" class="col-xs-12 col-md-offset-2 col-lg-offset-2 col-sm-offset-2 col-md-5 col-sm-5 col-lg-5 btn btn-large btn-danger"><i class="fa fa-sign-out"></i> Sign Out</a>
 	</div>
 </div>
