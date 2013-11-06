e1c2dd577d99aa3ac46ba0637ce5db589d8bdfda
Ben Woodford <ben@tweedler.co.uk>
2013-11-06 22:30:14 +0000

    Removed btn-block

--- fuel/app/views/sign.php
+++ fuel/app/views/sign.php
@@ -8,7 +8,7 @@
 	<div class="box-content text-center">
 		<div class="row">
 			<a href="/sign/in" class="col-md-6 col-sm-6 col-xs-12 col-lg-6 btn btn-large btn-success"><i class="fa fa-sign-in"></i> Sign In</a>
-			<a href="/sign/out" class="col-md-6 col-sm-6 col-xs-12 col-lg-6 btn btn-large btn-block btn-danger"><i class="fa fa-sign-out"></i> Sign Out</a>
+			<a href="/sign/out" class="col-md-6 col-sm-6 col-xs-12 col-lg-6 btn btn-large btn-danger"><i class="fa fa-sign-out"></i> Sign Out</a>
 		</div>
 	</div>
 </div>
