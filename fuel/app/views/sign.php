ca8d01a6534170cddd267f62a9fe81d5b9dad56a
Ben Woodford <ben@tweedler.co.uk>
2013-11-06 22:28:02 +0000

    Fixed icon

--- fuel/app/views/sign.php
+++ fuel/app/views/sign.php
@@ -6,7 +6,7 @@
 		</h2>
 	</div>
 	<div class="box-content text-center">
-		<a href="/sign/in" class="btn btn-large btn-success"><i class="fa-sign-in"></i> Sign In</a>
-		<a href="/sign/out" class="btn btn-large btn-danger"><i class="fa-sign-out"></i> Sign Out</a>
+		<a href="/sign/in" class="btn btn-large btn-success"><i class="fa fa-sign-in"></i> Sign In</a>
+		<a href="/sign/out" class="btn btn-large btn-danger"><i class="fa fa-sign-out"></i> Sign Out</a>
 	</div>
 </div>
