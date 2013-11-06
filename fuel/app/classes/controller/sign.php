25d6477eb18f225d6a8b22e8fb3d58a83199c9fe
Ben Woodford <ben@tweedler.co.uk>
2013-11-06 22:19:17 +0000

    Added sign in/out basics

---
+++ fuel/app/classes/controller/sign.php
@@ -0,0 +1,24 @@
+<?php
+
+class Controller_NoTicket extends Controller_Base
+{
+	public function action_index()
+	{
+		$view = View::forge('sign');
+		return Response::forge($view);
+	}
+
+	public function action_in() {
+		Model_Inout::SignIn($this->current_user->id);
+
+		Response::redirect('/sign');
+	}
+
+	public function action_out() {
+		Model_Inout::SignOut($this->current_user->id);
+
+		Response::redirect('/sign');
+	}
+}
+
+?>
