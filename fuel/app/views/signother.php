a391ea8f25ad79f5280005ee31ba9dccaa3e532a
Ben Woodford <ben@tweedler.co.uk>
2013-11-06 23:37:35 +0000

    Margin Right

--- fuel/app/views/signother.php
+++ fuel/app/views/signother.php
@@ -7,14 +7,14 @@
 		</h2>
 	</div>
 	<div class="box-content">
-		<img src="<?=$user->avatar_url;?>" class="pull-left" />
+		<img src="<?=$user->avatar_url;?>" class="pull-left" style="margin-right: 30px;" />
 
 		<p>
 			<strong>Username:</strong> <?=$user->username;?><br />
 			<strong>Student ID:</strong> <?=$user->student_number;?><br />
 			<strong>Checked in?</strong> <?=($user->hasCheckedIn() ? "Yes" : "<blink>No</blink");?><br />
 			<strong>Has Seat?</strong> <?=($user->hasSeat() ? "Yes" : "<blink>No</blink>");?><br />
-			<strong>Steam ID:</strong> <?=($user->steam);?><br />
+			<strong>Steam ID:</strong> <?=(strlen($user->steam)> 0 ? $user->steam : "Not Entered");?><br />
 		</p>
 	</div>
 </div>
